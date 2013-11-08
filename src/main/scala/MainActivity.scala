e509a5b0c827e03f344491d50f143797e9784030
Dan Osipov <dano@aweber.com>
2013-11-08 14:19:21 -0500

    Remove unused import

--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -5,7 +5,6 @@ import android.view.View
 import android.os.Bundle
 import android.widget.Toast
 import akka.actor._
-import akka.actor.LightArrayRevolverScheduler
 
 class MainActivity extends Activity with TypedActivity {
 
